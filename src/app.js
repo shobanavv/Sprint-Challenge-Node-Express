175345d031d59eb89815b313343194e3a8bf7a40
shobanavv <shobanavv@gmail.com>
2018-03-09 12:58:02 -0800

    completed MVP with the help of LO.  Working on MVC

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config.js');
-const URI_CURRENT_PRICE = 'https://api.coindesk.com/v1/bpi/currentprice.json?currency=USD'
+const URI_CURRENT_PRICE = 'https://api.coindesk.com/v1/bpi/currentprice/usd.json'
 const URI_PREVIOUS_DAY_PRICE = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
 const PORT = config.port;
 
@@ -30,39 +30,35 @@ const API_ARR = [URI_CURRENT_PRICE, URI_PREVIOUS_DAY_PRICE];
 
 app.get('/compare',(req,res) => {
   const bpiPromises = API_ARR.map(uri => {
-  fetch(uri)
-  .then(res => res.json())
-  .then(current => { // got places object.
-    console.log(current);
-    return current.bpi;
-  })
-    fetch(uri)
-  .then(res => res.json())
-  .then(res => {
-    console.log(res.bpi);
-    return res.bpi;
-  })
-  .catch(err =>{
-        res.status(422);
-        res.send({error:"error occured"});
-})
-     
-     Promise.all(bpiPromises) 
-     const comments = '';
-        if(bpiPromises[0]>bpiPromises[1]) return comments = 'raisen'
-        else return comments = 'fallen';
-     .then(comments => {
-      res.status(200);
-      res.send({result: comments})
-     })
-     .catch(err =>{
-        res.status(422);
-        res.send({error:"error occured"});
-    })
+       return fetch(uri)
+       .then(res => res.json())
+  }) //map
     
-})
-  
+  // .catch(err =>{
+  //       res.status(422);
+  //       res.send({error:"error occured"});
+  // })
 
+
+       Promise.all(bpiPromises) 
+       .then(comments => {
+        let todayRate = comments[0].bpi.USD.rate.replace(/\,/g,'');
+        let yesterRate = Object.values(comments[1].bpi)[0];
+        todayRate = parseInt(todayRate,10);
+        yesterRate = parseInt(yesterRate,10);
+        difference = todayRate - yesterRate;
+        let result ="";
+        if(difference > 0) result = `Raisen by ${difference}`; 
+        else result = `Fallen by ${difference}`;
+        res.status(200);
+        res.send({result: result });
+       })
+       .catch(err =>{
+          res.status(422);
+          res.send({error:"error occured"});
+      })
+    
+}); //get
 
 app.listen(PORT, err => {
     if(err) {
